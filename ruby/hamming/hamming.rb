610814b46795710ed015e4e76df33f2b2f403fa2
Edward McCarthy <efmccarthy26@gmail.com>
2015-01-15 14:27:45 -0700

    separated methods. Did if else without ternary

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,19 +1,35 @@
 class Hamming
 
   def self.compute(strand1, strand2)
-    counter = 0
-    
-    split1 = strand1.chars
-    split2 = strand2.chars
-    set_of_strands = split1.zip(split2)
-    set_of_strands.each do |set|
-      if set[0] != set[1]
-        counter += 1
-      elsif
-        0
-      end
-    end
-    counter
+    zipped_strands = strand1.chars.zip(strand2.chars)
+    zipped_strands.flatten
+    count(zipped_strands)
   end
 
+  def self.count(zipped_strands)
+    zipped_strands.count do |x,y|
+      if y == nil || x == nil
+        nil
+      else
+        x != y
+      end
+    end
+  end
 end
+
+## Original ##
+
+# def self.compute(strand1, strand2)
+  # counter = 0
+  # split1 = strand1.chars
+  # split2 = strand2.chars
+  # set_of_strands = split1.zip(split2)
+  # set_of_strands.each do |set|
+  #   if set[0] != set[1]
+  #     counter += 1
+  #   elsif
+  #     0
+  #   end
+  # end
+  # counter
+# end
--- ruby/hamming/hamming_test.rb
+++ ruby/hamming/hamming_test.rb
@@ -27,17 +27,14 @@ class HammingTest < MiniTest::Unit::TestCase
   end
 
   def test_ignores_extra_length_on_other_strand_when_longer
-    skip
     assert_equal 2, Hamming.compute('AGG', 'AAAACTGACCCACCCCAGG')
   end
 
   def test_large_hamming_distance
-    skip
     assert_equal 4, Hamming.compute('GATACA', 'GCATAA')
   end
 
   def test_hamming_distance_in_very_long_strand
-    skip
     assert_equal 9, Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
   end
 end
