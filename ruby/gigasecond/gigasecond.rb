3f274d621b2ecabdee6a2509d4c76fcd9943bab4
Edward McCarthy <efmccarthy26@gmail.com>
2015-01-19 15:54:47 -0700

    didn't do last one. whatever

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,8 +1,11 @@
 class Gigasecond
 
-  def self.from(date)
-    date + ((10**9)/60/60/24)
+  def self.from(input)
+    if input.is_a?(Time)
+      (input + (10**9)).to_date
+    else
+      input + ((10**9)/60/60/24)
+    end
   end
-
 
 end
